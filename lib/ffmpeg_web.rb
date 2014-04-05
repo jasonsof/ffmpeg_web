c3e6758dca955e6d9927d3bef1879d232f4d3155
Jason <jsofokleous@googlemail.com>
2014-04-05 10:07:09 +0100

    prepare the transcoder for a new job

--- lib/ffmpeg_web.rb
+++ lib/ffmpeg_web.rb
@@ -1,5 +1,14 @@
 require "ffmpeg_web/version"
 
 module FFmpegWeb
-  # Your code goes here...
+  class Transcoder
+    attr_accessor :input, :output
+
+    def initialize(input, output)
+      raise "Input file does not exist" if !File.exists?(input)
+      raise "Output directory does not exist" if !File.exists?(File.dirname(output))
+      @input = input
+      @output = output
+    end
+  end
 end
--- spec/transcoder_spec.rb
+++ spec/transcoder_spec.rb
@@ -1,1 +1,48 @@
 require 'spec_helper'
+
+describe FFmpegWeb::Transcoder do
+  describe "#initialize" do
+    it "sets the @input variable" do
+      input = "/path/to/input.mov"
+      output = "/path/to/output"
+
+      File.stub(:exists?).with(input).and_return(true)
+      File.stub(:exists?).with("/path/to").and_return(true)
+
+      transcoder = FFmpegWeb::Transcoder.new(input, output)
+      expect(transcoder.instance_variable_get(:@input)).to eq input
+    end
+    it "sets the @output variable" do
+      input = "/path/to/input.mov"
+      output = "/path/to/output"
+
+      File.stub(:exists?).with(input).and_return(true)
+      File.stub(:exists?).with("/path/to").and_return(true)
+
+      transcoder = FFmpegWeb::Transcoder.new(input, output)
+      expect(transcoder.instance_variable_get(:@output)).to eq output
+    end
+
+    context "when the input does not exist" do
+      it "raises an exception" do
+        input = "/path/to/nonexistent/input.mov"
+        output = "/path/to/output"
+
+        expect {
+          transcoder = FFmpegWeb::Transcoder.new(input, output)
+        }.to raise_exception
+      end
+    end
+
+    context "when the output directory does not exist" do
+      it "raises an exception" do
+        input = "/path/to/input.mov"
+        output = "/path/nonexistent/output"
+
+        expect {
+          transcoder = FFmpegWeb::Transcoder.new(input, output)
+        }.to raise_exception
+      end
+    end
+  end
+end
