6b18d641b3f84cfb735cc0b2e423d72aa3b28704
Jason <jsofokleous@googlemail.com>
2014-04-05 12:03:08 +0100

    DRY'd up the transcoder class a bit

--- lib/ffmpeg_web.rb
+++ lib/ffmpeg_web.rb
@@ -2,56 +2,49 @@ require "ffmpeg_web/version"
 
 module FFmpegWeb
   class Transcoder
-    attr_accessor :input, :output
+    attr_accessor :input, :output, :video_info
 
     def initialize(input, output)
-      raise "Input file does not exist" if !File.exists?(input)
-      raise "Output directory does not exist" if !File.exists?(File.dirname(output))
-      raise "File is not a valid movie file" if !is_valid?(input)
-
       @input = input
       @output = output
+      
+      raise "Input file does not exist" if !File.exists?(input)
+      raise "Output directory does not exist" if !File.exists?(File.dirname(output))
+
+      @video_info = ffmpeg_info
+
+      raise "File is not a valid movie file" if !is_valid?
     end
 
     # returns the duration of the video in seconds
     def duration
-      read, write = IO::pipe
-      pid = Process.fork do
-        read.close
-
-        $stderr.reopen(write)
-        exec("ffmpeg -y -i '#{@input}'")
-      end
-      write.close
-
-      #Process.wait(pid)
-
-      ffmpeg_output = read.read
-      duration = /Duration: (\d{2}):(\d{2}):(\d{2}).(\d{2})/.match(ffmpeg_output)
+      duration = /Duration: (\d{2}):(\d{2}):(\d{2}).(\d{2})/.match(@video_info)
       duration_in_milliseconds = ($~[1].to_i * 60 * 60) + ($~[2].to_i * 60) + $~[3].to_i + ($~[4].to_i / 1000)
     end
 
     private
-      def is_valid?(input)
-        video_stream, audio_stream = nil
-
+      def ffmpeg_info
         read, write = IO::pipe
         pid = Process.fork do
           read.close
 
           $stderr.reopen(write)
-          exec("ffmpeg -y -i '#{input}'")
+          exec("ffmpeg -y -i '#{@input}'")
         end
         write.close
 
-        #Process.wait(pid)
-
         ffmpeg_output = read.read
-        if match = ffmpeg_output.match(/Video: (.*)$/i)
+        ffmpeg_output
+      end
+
+      def is_valid?
+        video_stream, audio_stream = nil
+
+        if match = @video_info.match(/Video: (.*)$/i)
           video_stream = match.captures.first
         end
 
-        if match = ffmpeg_output.match(/Audio: (.*)$/i)
+        if match = @video_info.match(/Audio: (.*)$/i)
           audio_stream = match.captures.first
         end
 
