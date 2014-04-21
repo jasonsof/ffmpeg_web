2ca7e615d81420aaf078039cfc1129e45a9d7603
Jason <jsofokleous@googlemail.com>
2014-04-21 19:01:15 +0100

    refactored the spec file

--- spec/transcoder_spec.rb
+++ spec/transcoder_spec.rb
@@ -1,29 +1,24 @@
 require 'spec_helper'
 
 describe FFmpegWeb::Transcoder do
+  let(:good_input) { "#{fixture_path}/10 seconds.mov" }
+  let(:bad_input) { "#{fixture_path}/broken.mp4" }
+  let(:output) { "#{fixture_path}/output" }
+
   describe "#initialize" do
     it "sets the @input variable" do
-      input = "#{fixture_path}/10 seconds.mov"
-      output = "#{fixture_path}/output.mp4"
-
-      transcoder = FFmpegWeb::Transcoder.new(input, output)
-      expect(transcoder.instance_variable_get(:@input)).to eq input
+      transcoder = FFmpegWeb::Transcoder.new(good_input, output)
+      expect(transcoder.instance_variable_get(:@input)).to eq good_input
     end
     it "sets the @output variable" do
-      input = "#{fixture_path}/10 seconds.mov"
-      output = "#{fixture_path}/output.mp4"
-
-      transcoder = FFmpegWeb::Transcoder.new(input, output)
+      transcoder = FFmpegWeb::Transcoder.new(good_input, output)
       expect(transcoder.instance_variable_get(:@output)).to eq output
     end
 
     context "when the input is an invalid video file" do
       it "raises an exception" do
-        input = "#{fixture_path}/broken.mp4"
-        output = "#{fixture_path}/output.mp4"
-
         expect {
-          transcoder = FFmpegWeb::Transcoder.new(input, output)
+          transcoder = FFmpegWeb::Transcoder.new(bad_input, output)
           }.to raise_exception
       end
     end
@@ -31,7 +26,6 @@ describe FFmpegWeb::Transcoder do
     context "when the input does not exist" do
       it "raises an exception" do
         input = "/path/to/nonexistent/input.mov"
-        output = "/path/to/output"
 
         expect {
           transcoder = FFmpegWeb::Transcoder.new(input, output)
@@ -41,11 +35,10 @@ describe FFmpegWeb::Transcoder do
 
     context "when the output directory does not exist" do
       it "raises an exception" do
-        input = "/path/to/input.mov"
         output = "/path/nonexistent/output"
 
         expect {
-          transcoder = FFmpegWeb::Transcoder.new(input, output)
+          transcoder = FFmpegWeb::Transcoder.new(good_input, output)
         }.to raise_exception
       end
     end
@@ -54,18 +47,12 @@ describe FFmpegWeb::Transcoder do
   describe "#duration" do
     context "when the input is a valid video file" do
       it "returns the videos duration in seconds" do
-        input = "#{fixture_path}/10 seconds.mov"
-        output = "#{fixture_path}/output.mp4"
-
-        transcoder = FFmpegWeb::Transcoder.new(input, output)
+        transcoder = FFmpegWeb::Transcoder.new(good_input, output)
         expect(transcoder.duration).to eq(10)
       end
 
       it "returns a Fixnum" do
-        input = "#{fixture_path}/10 seconds.mov"
-        output = "#{fixture_path}/output.mp4"
-
-        transcoder = FFmpegWeb::Transcoder.new(input, output)
+        transcoder = FFmpegWeb::Transcoder.new(good_input, output)
         expect(transcoder.duration).to be_a(Fixnum)
       end
     end
@@ -85,11 +72,9 @@ describe FFmpegWeb::Transcoder do
 
     context "given an output filesize" do
       it "creates an output file around the size given" do
-        input = "#{fixture_path}/10 seconds.mov"
-        output = "#{fixture_path}/output"
         desired_output_size_in_mb = 10
 
-        transcoder = FFmpegWeb::Transcoder.new(input, output)
+        transcoder = FFmpegWeb::Transcoder.new(good_input, output)
         progress_stream = transcoder.transcode(desired_output_size_in_mb)
         while progress = progress_stream.gets
         end
@@ -102,10 +87,7 @@ describe FFmpegWeb::Transcoder do
 
     context "without given an output filesize" do
       it "creates an output file" do
-        input = "#{fixture_path}/10 seconds.mov"
-        output = "#{fixture_path}/output"
-
-        transcoder = FFmpegWeb::Transcoder.new(input, output)
+        transcoder = FFmpegWeb::Transcoder.new(good_input, output)
         progress_stream = transcoder.transcode(10)
         while progress = progress_stream.gets
         end
@@ -115,10 +97,7 @@ describe FFmpegWeb::Transcoder do
     end
 
     it "returns an IO stream" do
-      input = "#{fixture_path}/10 seconds.mov"
-      output = "#{fixture_path}/output"
-
-      transcoder = FFmpegWeb::Transcoder.new(input, output)
+      transcoder = FFmpegWeb::Transcoder.new(good_input, output)
       progress_stream = transcoder.transcode(10)
       expect(progress_stream).to be_an(IO)
     end
